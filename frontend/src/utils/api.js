cc564ee6ac4f6c839d6da040acd5372fdc55abe3
Alex Beltyukov <xonika9@gmail.com>
2022-09-28 17:05:41 +0300

    add credentials field

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -4,61 +4,64 @@ class Api {
     this._headers = headers;
   }
   _checkResponse = (res) => {
-    if (res.ok) {
-      return res.json();
-    }
-    return Promise.reject(`Ошибка: ${res.status}`);
-  };
+    return res.ok ? res.json() : Promise.reject(`Ошибка: ${res.status}`);
+  }
   setAvatar = (formValues) => {
-    return fetch(`${this._baseUrl}users/me/avatar`, {
+    return fetch(`${this._baseUrl}/users/me/avatar`, {
       method: 'PATCH',
+      credentials: 'include',
       headers: this._headers,
       body: JSON.stringify({ avatar: formValues.avatar }),
     }).then(this._checkResponse);
   };
   getUserInfo = () => {
-    return fetch(`${this._baseUrl}users/me`, {
+    return fetch(`${this._baseUrl}/users/me`, {
+      credentials: 'include',
       headers: this._headers,
     }).then(this._checkResponse);
   };
   setUserInfo = (profileInfo) => {
-    return fetch(`${this._baseUrl}users/me`, {
+    return fetch(`${this._baseUrl}/users/me`, {
       method: 'PATCH',
+      credentials: 'include',
       headers: this._headers,
       body: JSON.stringify(profileInfo),
     }).then(this._checkResponse);
   };
   getInitialCards = () => {
-    return fetch(`${this._baseUrl}cards`, {
+    return fetch(`${this._baseUrl}/cards`, {
+      credentials: 'include',
       headers: this._headers,
     }).then(this._checkResponse);
   };
   addCard = (formValues) => {
-    return fetch(`${this._baseUrl}cards`, {
+    return fetch(`${this._baseUrl}/cards`, {
       method: 'POST',
+      credentials: 'include',
       headers: this._headers,
       body: JSON.stringify({ name: formValues.title, link: formValues.link }),
     }).then(this._checkResponse);
   };
   removeCard = (cardId) => {
-    return fetch(`${this._baseUrl}cards/${cardId}`, {
+    return fetch(`${this._baseUrl}/cards/${cardId}`, {
       method: 'DELETE',
+      credentials: 'include',
       headers: this._headers,
     }).then(this._checkResponse);
   };
   toggleLike = (cardId, isLiked) => {
     return fetch(`${this._baseUrl}/cards/${cardId}/likes`, {
-      method: `${isLiked ? 'DELETE' : 'PUT'}`,
+      method: `${isLiked ? "DELETE" : "PUT"}`,
+      credentials: "include",
       headers: this._headers,
     }).then(this._checkResponse);
   };
 }
 const api = new Api({
-  baseUrl: 'http://localhost:4000/',
+  baseUrl: 'http://localhost:4000',
   headers: {
-    authorization: '3561314d-9c6f-4e96-b81e-d301e13da90c',
     'Content-Type': 'application/json',
   },
 });
-//changes
+
 export default api;
