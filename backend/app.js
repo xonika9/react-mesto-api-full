d8da4d7fe38660a3680564a8a13f25297108aca8
xonika9 <xonika9@gmail.com>
2023-02-20 14:31:57 +0300

    fix for server

--- backend/app.js
+++ backend/app.js
@@ -8,14 +8,14 @@ const router = require('./routes/index');
 const errorHandler = require('./middlewares/error');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3001 } = process.env;
 
 const app = express();
 
 app.use(
   cors({
     origin: [
-      'http://localhost:3000',
+      'http://localhost:3001',
       'http://mesto.xonika.ru',
       'https://mesto.xonika.ru',
     ],
